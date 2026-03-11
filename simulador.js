1c962f8d3606ac8809412b772a14f85edb748bd2
Ignacio Bruno <ignaciobruno90@gmail.com>
2026-03-11 13:56:05 -0300

    Probando indicadores

--- simulador.js
+++ simulador.js
@@ -78,59 +78,38 @@ function simular() {
 
 `;
 }
-// function calcularCuota(capital, tasaAnual, cuotas) {
 
-//     const tasaMensual = tasaAnual / 12 / 100;
+async function obtenerDolar() {
 
-//     const cuota = capital *
-//         (tasaMensual * Math.pow(1 + tasaMensual, cuotas)) /
-//         (Math.pow(1 + tasaMensual, cuotas) - 1);
+    const response = await fetch("https://dolarapi.com/v1/dolares/oficial");
+    const data = await response.json();
 
-//     return cuota;
-// }
+    return data.venta;
+}
 
-// function generarTabla(capital, tasaAnual, cuotas){
+async function obtenerUVA() {
 
-//     const tasaMensual = tasaAnual / 12 / 100;
-//     const cuota = calcularCuota(capital, tasaAnual, cuotas);
+    const response = await fetch("https://dolarapi.com/v1/uva");
+    const data = await response.json();
 
-//     let saldo = capital;
-//     let tabla = [];
+    return data.valor;
+}
 
-//     for(let i = 1; i <= cuotas; i++){
+async function cargarIndicadores() {
 
-//         let interes = saldo * tasaMensual;
-//         let capitalPagado = cuota - interes;
+    try {
 
-//         saldo -= capitalPagado;
+        const dolar = await obtenerDolar();
+        const uva = await obtenerUVA();
 
-//         tabla.push({
-//             cuota: i,
-//             interes: interes,
-//             capital: capitalPagado,
-//             saldo: saldo
-//         });
-//     }
+        document.getElementById("dolar-oficial").innerText = dolar.toFixed(2);
+        document.getElementById("uva").innerText = uva.toFixed(2);
 
-//     return tabla;
-// }
+    } catch (error) {
 
-// function simular(){
+        console.error("Error cargando indicadores", error);
 
-//     const saldo = parseFloat(document.getElementById("saldo").value);
-//     const tasa = parseFloat(document.getElementById("tasa").value);
-//     const cuotas = parseInt(document.getElementById("cuotas").value);
-//     const prepago = parseFloat(document.getElementById("prepago").value);
+    }
+}
 
-//     const saldoNuevo = saldo - prepago;
-
-//     const cuotaActual = calcularCuota(saldo, tasa, cuotas);
-//     const cuotaNueva = calcularCuota(saldoNuevo, tasa, cuotas);
-
-//     document.getElementById("resultado").innerHTML =
-//     `
-//     Cuota actual: ${cuotaActual.toFixed(2)} UVA <br>
-//     Nueva cuota: ${cuotaNueva.toFixed(2)} UVA <br>
-//     Nuevo saldo: ${saldoNuevo.toFixed(2)} UVA
-//     `;
-// }
+cargarIndicadores();
